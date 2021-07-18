8b5a137a7d3252a164158df287e505eff16a9d81
Chris Wilding <hello@chriswilding.co.uk>
2021-07-18 18:33:17 +0100

    Add dynamodb table

---
+++ infrastructure/dynamodb.tf
@@ -0,0 +1,25 @@
+resource "aws_dynamodb_table" "ginventory" {
+  name           = "ginventory"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "PK"
+  range_key      = "SK"
+
+  attribute {
+    name = "PK"
+    type = "S"
+  }
+
+  attribute {
+    name = "SK"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "TTL"
+    enabled        = true
+  }
+
+  server_side_encryption {
+      enabled = true
+  }
+}
